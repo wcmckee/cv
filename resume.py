ce7381565ca5c77ab10c63e8a636e8f99403135f
wcmckee <will@artcontrol.me>
2015-01-25 14:23:49 +1300

    convert to html/py

---
+++ resume.py
@@ -0,0 +1,68 @@
+
+# coding: utf-8
+
+# <h3>William Mckee</h3>
+# 
+# Email: will (at) artcontrol.me
+# 
+# Phone: 0223721475
+# 
+# <h3>Education</h3>
+# 
+# Diploma with Honors in Art and Creativity. The Learning Connection. July 2010 - December 2012.
+# 
+# Studied Drawing and Design. Palmerston North School Of Design. October 2008 - July 2010.
+# 
+# Studied Applied Visual Imaging. Palmerston North UCOL. 2007 - 2009.
+# 
+# NCEA Level 2. Horowhenua Collage, Levin. 2002 - 2006
+# 
+# <h3>Work</h3>
+# 
+# Education Support Worker, Ministery of Education. Working at Whaihanga Early Learning Centre. October 2014 - December 2014.
+# 
+# Video Game Development tutor. Chalkle. September 2013.
+# 
+# Casual Life modeling. The Learning Connection. 2011 - 2012.
+# 
+# Mail Delivery. Kiwi Mail. 2004
+# 
+# <h3>Volunteer</h3>
+# 
+# linux.conf.au. January 10-16 2015. AV team - recording speakers talks. http://linux.conf.au/
+# 
+# Te Whare O Te Ata (Fairfield Community Centre). Feburary 2014 - September 2014. Working with children on IT and art. Learning Linux sysadmin. http://fairfield.org.nz/
+# 
+# Python Weekly Classes. 2014. Te Whare O Te Ata. Wednesday night programming and computer help.
+# 
+# Whaihanga Early Learning Centre. 2014. Helper. Areas of note: art and building with blocks. Assistence with morning walks. 
+# 
+# SeniorNet, Levin. 2013. Helped eldely with their digital devices.  
+# 
+# Company Branding Shop. 2010. Screen printing. http://shirt.co.nz
+# 
+# <h3>Speaker</h3>
+# 
+# linux.conf.au. Jan 2015. Lightning Talk at astro miniconf regarding IPython Notebook. http://bit.ly/15zGtNC
+# 
+# Hamilton Computer Club. 2013 - 2014. Involvment and speaker Feb 2014 on Python programming language.
+# 
+# Hamilton Linux Users Group. 2014. Speaker on GoDot Game Engine.  
+# 
+# Kiwi PyCon 2013. Lightning Talk regarding lastfm api. http://bit.ly/18dgBJb
+# 
+# <h3>Personal Projects</h3>
+# 
+# ArtControl.me: The Art Of William Mckee. 2010 - Ongoing. Blog for uploading and discussing artwork. Pencil Drawing, Digital Painting. Life drawing, portraits, street scenes, landscapes. http://artcontrol.me CC:BY licence.
+# 
+# BroBeur Studios: Video Game Development. 2012 - 2013. 13 game on Google Play Store. Majority solo, created between 48hours and 7days. Collab with others in small 2-4 people teams. Involve in Global Game Jam 2013, 2014, 2015. http://brobeur.com
+# 
+# FreshFigure Photography. 2012 - Ongoing. Stock Photography site to host photography. Street and landscapes. Photography used by Horowhenua Mail, March 2013. http://freshfigure.com/art CC:BY licence.
+# 
+# WCMCKEE: Web and Software Development. 2011 - Ongoing. Pythong programming. Web scalping, data processing, website generation. Notable sites/scripts generatored include http://getsdrawn.com and LCA Signin (digital signin/out system). http://wcmckee.com MIT licence
+# 
+
+# In[ ]:
+
+
+
