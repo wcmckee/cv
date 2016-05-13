8c7da7497eef50c8c53a8458d539b90b141fa27a
William Mckee <will@artcontrol.me>
2016-05-14 01:21:17 +1200

    update may

--- resume.py
+++ resume.py
@@ -1,27 +1,24 @@
 
 # coding: utf-8
 
-# <h3>William C Mckee</h3>
-# 
+# William C Mckee
 # Email: will (at) artcontrol.me
-# 
 # Phone: 0223721475
 # 
-# Concept art, 3d modeling, video game development, earlychildhood education, open-source software development, photography, long walks, cooking...
+# Growing up in Levin, William now lives in Hamilton. Formally trained in visual arts and self-taught in programming and writing. Passion for public speaking.
+# Education
 # 
-# <h3>Education</h3>
-# 
-# Diploma with Honors in Art and Creativity. The Learning Connection. July 2010 - December 2012.
+# Diploma with Honors in Art and Creativity. The Learning Connexion. July 2010 - December 2012.
 # 
 # Studied Drawing and Design. Palmerston North School Of Design. October 2008 - July 2010.
 # 
 # Studied Applied Visual Imaging. Palmerston North UCOL. 2008 - 2009.
 # 
-# NCEA Level 2. Horowhenua Collage, Levin. 2002 - 2006
+# NCEA Level 2. Horowhenua College, Levin. 2002 - 2006
 # 
-# <h3>Work</h3>
+# Work
 # 
-# Education Support Worker, Ministery of Education. Working at Whaihanga Early Learning Centre. October 2014 - December 2014.
+# Education Support Worker, Ministry of Education. Working at Whaihanga Early Learning Centre. October 2014 - December 2014.
 # 
 # Video Game Development tutor. Chalkle. September 2013.
 # 
@@ -29,37 +26,54 @@
 # 
 # Mail Delivery. Kiwi Mail. 2005.
 # 
-# <h3>Volunteer</h3>
+# Volunteer
+# 
+# Writers Den Hamilton - 2015 - Ongoing. Website development
+# 
+# Creative Commons Aotearoa New Zealand - July 2015 - Ongoing. Speaker, web development. 
+# 
+# Hamilton Library - 2015 - 2016 Computer Mentor 
+# 
+# KiwiPyCon2015. September 2015. AV team - recording talks.
+# 
+# KiwiJam2015. July 2015. Event Photographer.
 # 
 # linux.conf.au. January 10-16 2015. AV team - recording talks. http://linux.conf.au/
 # 
-# Te Whare O Te Ata (Fairfield Community Centre). Feburary 2014 - September 2014. Working with children on IT and art. Learning Linux sysadmin. http://fairfield.org.nz/
+# Te Whare O Te Ata (Fairfield Community Centre). February 2014 - September 2014. Working with children on IT and art. Learning Linux sysadmin. http://fairfield.org.nz/
 # 
 # Python Weekly Classes. 2014. Te Whare O Te Ata. Wednesday night programming and computer help.
 # 
-# Whaihanga Early Learning Centre. 2014. Volunteer helper. Art, building with blocks and morning walks. 
+# Whaihanga Early Learning Centre. 2014. Volunteer helper. Art, building with blocks and morning walks.
 # 
-# SeniorNet, Levin. 2013. Helped eldely with their digital devices.  
+# SeniorNet, Levin. 2013. Helped elderly with their digital devices.
 # 
 # Company Branding Shop. 2010. Screen printing. http://shirt.co.nz
 # 
-# <h3>Speaker</h3>
+# Speaker
+# 
+# Hamilton Python User Group. 2014 - Ongoing. 
+# 
+# KiwiPyCon 2013/2015. Lightning talk speaker.
 # 
 # linux.conf.au. Jan 2015. Lightning Talk at astro miniconf regarding IPython Notebook. http://bit.ly/15zGtNC
 # 
 # Hamilton Computer Club. 2013 - 2014. Member and speaker Feb 2014 on Python programming language.
 # 
-# Hamilton Linux Users Group. 2014. Speaker on GoDot Game Engine.  
+# Hamilton Linux Users Group. 2014. Speaker on GoDot Game Engine.
 # 
-# Kiwi PyCon 2013. Lightning Talk regarding lastfm api. http://bit.ly/18dgBJb
+# Projects
 # 
-# <h3>Projects</h3>
+# ArtCtrl.me: The Art Of William Mckee. 2010 - 2015. Upload and discussion of artwork. Pencil and digital drawing, life drawing, portraits, street, and landscapes. http://artctrl.me.
 # 
-# ArtControl.me: The Art Of William Mckee. 2010 - Ongoing. Blog for uploading and discussing artwork. Pencil Drawing, Digital Painting. Life drawing, portraits, street scenes, landscapes. http://artcontrol.me CC:BY licence.
+# BroBeur Studios: Video Game Development. 2012 - 2013. 13 game on Google Play Store. Majority solo, created between 48 hours and 7days. Collab with others in small 2-4 people teams. Involve in Global Game Jam 2013, 2014, 2015.
 # 
-# BroBeur Studios: Video Game Development. 2012 - 2013. 13 game on Google Play Store. Majority solo, created between 48hours and 7days. Collab with others in small 2-4 people teams. Involve in Global Game Jam 2013, 2014, 2015. http://brobeur.com
+# FreshFigure Photography. 2012 - 2013. Photography used by Horowhenua Mail, March 2013.
 # 
-# FreshFigure Photography. 2012 - Ongoing. Stock Photography site to host photography. Street and landscapes. Photography used by Horowhenua Mail, March 2013. http://freshfigure.com/art CC:BY licence.
+# WCMCKEE: Web and Software Development. 2011 - Ongoing. http://wcmckee.com
 # 
-# WCMCKEE: Web and Software Development. 2011 - Ongoing. Python programming. Web scalping, data processing, website generation, point of sale, cybercafe managment system. Sites generatored include http://getsdrawn.com and LCA Signin (digital signin/out system). http://wcmckee.com MIT licence.
-# 
+
+# In[ ]:
+
+
+
